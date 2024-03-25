611f2dc810b1cd0ae46172addbd33a7fa846ec8d
Adiam Ahmed <adu.adu911@gmail.com>
2024-03-25 04:39:36 -0400

    Create band-site-api.js to handle API interaction. A class has been defined with a constructor method to store the API key and base URL.

---
+++ scripts/band-site-api.js
@@ -0,0 +1,11 @@
+const apiKey = "ad131f42-c497-46c7-9e46-41dca19a8e15";
+
+class NewBandsiteApi {
+    constructor(apiKey) {
+        this.apiKey = apiKey;
+        this.baseUrl = 'https://unit-2-project-api-25c1595833b2.herokuapp.com/';
+    }
+};
+
+
+
